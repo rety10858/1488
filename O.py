df3a8d7b899ada7d6fffdd3ebe78790d80134f84
rety10858 <rety10858@gmail.com>
2024-10-04 18:08:39 +0000

    12345

--- O.py
+++ O.py
@@ -1,9 +1,9 @@
-a = [1, 3, 6]
+
 print(a[0])
 
 b = [9, 10, 12]
 c = a+b
-print(c)
+
 
 print(c*2)
 c.append(2)
---
+++ homework.py
@@ -0,0 +1,17 @@
+z = []
+a = input('Введите возраст ')
+b = 'Возраст: ' + a
+z.append(b)
+o = input('Укажите пол ')
+i = 'Пол: ' + o
+z.append(i)
+k = input('Укажите имя ')
+m = 'Имя: ' + k
+z.append(m)
+v = input('Укажите город в котором проживаете ')
+pp = 'Город: ' + v
+z.append(pp)
+q = input('Укажите класс обучения ')
+qq = 'Класс: ' + q
+z.append(qq)
+print(z)
